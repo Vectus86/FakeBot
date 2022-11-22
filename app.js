1df48eeda97422cc510e6f3a07168aeea286f06a
calvinlawrence88 <calvinlawrence673@gmail.com>
2022-11-22 20:05:43 +0800

    FakeBot

---
+++ app.js
@@ -0,0 +1,53 @@
+const Answer = document.getElementById('Answer');
+const Question = document.getElementById('Question');
+
+let init = 0;
+
+const botSay = (data) => {
+  return [
+    'Halo Nama Kamu Siapa ?',
+    `Halo ${data?.Nama}, Berapa Usia Kamu ?`,
+    `Ohhh ${data?.Usia}, Hobi Kamu Apa Ya ?`,
+    `Waw Sama Dong Aku Juga Hobi ${data?.Hobi}, Sekarang Kamu Punya Pacar Gak ?`,
+    `Ohhh ${data?.Pacar}, Kalau Gitu Udahan Dulu Ya ?`,
+  ];
+};
+
+let userData = [];
+
+Question.innerHTML = botSay()[0];
+
+function botStart() {
+  init++;
+  if (init === 1) {
+    botDelay({ Nama: Answer.value });
+  } else if (init === 2) {
+    botDelay({ Usia: Answer.value });
+  } else if (init === 3) {
+    botDelay({ Hobi: Answer.value });
+  } else if (init === 4) {
+    botDelay({ Pacar: Answer.value });
+  } else if (init === 5) {
+    Finish();
+  } else {
+    botEnd();
+  }
+}
+
+function botDelay(jawabanUser) {
+  console.log({ userData: userData });
+  setTimeout(() => {
+    Question.innerHTML = botSay(jawabanUser)[init];
+  }, [1250]);
+  userData.push(Answer.value);
+  Answer.value = '';
+}
+
+function Finish() {
+  Question.innerHTML = `Thanks ${userData[0]} Udah Main Di FAKE BOT ini 😊`;
+  Answer.value = 'Siapp';
+}
+
+function botEnd() {
+  window.location.reload();
+}
